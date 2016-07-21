eeac85dd0c1f13c2a3a4a9cebcd7a2ad9b69fef5
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-07-20 23:01:51 -0700

    First pass at apolloAccounts default function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { isEmpty, isString, isObject, isArray, trim } from 'lodash';
+
 export class Accounts {
   serializeUser(user, callback) {
     return callback(null, user);
@@ -5,6 +7,12 @@ export class Accounts {
   deserializeUser(object, callback) {
     return callback(null, object);
   }
+  /**
+   * Returns a promise that calls Passport's verify callback with the results this.authenticate
+   * @param  {...object} ...args An array of objects, the last of which must be Passport's
+   * verify callback.
+   * @return Promise
+   */
   verify(...args) {
     const done = args.pop();
     return this.authenticate(...args).then(value => {
@@ -23,3 +31,25 @@ export class Accounts {
     });
   }
 }
+
+export default (passport, accounts, strategies) => {
+  if (!passport) {
+    throw new Error('Expects a passport instance');
+  }
+  if (!accounts) {
+    throw new Error('Expects an Accounts instance');
+  }
+  const newStrategies = isArray(strategies) ? strategies : [strategies];
+  newStrategies.forEach(({ name, strategy, options = {}, verify, findOrCreate }) => {
+    if (isEmpty(name) || isString(name) && trim(name).length === 0) {
+      throw new Error('Expects strategy to have a name');
+    }
+    if (isEmpty(strategy) && !isObject(strategy)) {
+      throw new Error('Expects a passport strategy');
+    }
+    passport.use(name, new strategy(options), accounts.verify); // eslint-disable-line new-cap
+  });
+  passport.serializeUser(accounts.serializeUser);
+  passport.deserializeUser(accounts.deserializeUser);
+  return passport;
+};
--- test/tests.js
+++ test/tests.js
@@ -2,10 +2,57 @@ import chai, { expect } from 'chai';
 import spies from 'chai-spies';
 import chaiAsPromised from 'chai-as-promised';
 
-import { Accounts } from '../src/';
+import apolloAccounts, { Accounts } from '../src/';
 
 chai.use(spies);
 chai.use(chaiAsPromised);
+
+describe('apolloAccounts', () => {
+  const passport = {
+    serializeUser() {
+
+    },
+    deserializeUser() {
+
+    },
+    use() {
+
+    },
+  };
+  const accounts = {
+    verify() {
+
+    },
+  };
+  const strategy = class {
+
+  };
+  it('expects passport', () => {
+    expect(() => apolloAccounts()).to.throw('Expects a passport instance');
+  });
+  it('expects accounts', () => {
+    expect(() => apolloAccounts(passport).to.throw('Expects an Accounts instance'));
+  });
+  it('expects a strategy name', () => {
+    expect(() => apolloAccounts(passport, accounts, { strategy: {} }))
+      .to.throw('Expects strategy to have a name');
+    expect(() => apolloAccounts(passport, accounts, { name: '', strategy }))
+      .to.throw('Expects strategy to have a name');
+    expect(() => apolloAccounts(passport, accounts, { name: ' ', strategy }))
+      .to.throw('Expects strategy to have a name');
+  });
+  it('expects a strategy', () => {
+    expect(() => apolloAccounts(passport, accounts, { name: 'name' }))
+      .to.throw('Expects a passport strategy');
+  });
+  it('can accept an array of strategies', () => {
+    passport.use = chai.spy(passport.use);
+    apolloAccounts(passport, accounts, [
+      { name: 'name1', strategy }, { name: 'name2', strategy },
+    ]);
+    expect(passport.use).to.have.been.called.twice();
+  });
+});
 
 describe('Accounts', () => {
   const accounts = new Accounts();
@@ -20,6 +67,20 @@ describe('Accounts', () => {
     const user = {};
     accounts.deserializeUser(user, callback);
     expect(callback).to.have.been.called.with(user);
+  });
+  describe('verify', () => {
+    it('verifies authentication is successful', (done) => {
+      const user = {};
+      accounts.authenticate = () => {
+        return new Promise((resolve) => resolve(user));
+      };
+      const callback = chai.spy((err, user) => {
+        console.log(user);
+        expect(callback).to.have.been.called.with(user);
+        done();
+      });
+      const verify = accounts.verify('username', 'password', callback);
+    });
   });
   /*
   describe('verify', () => {
