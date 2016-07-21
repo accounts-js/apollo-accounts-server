0d21d5003b6c21f9f7c2bb6c22002a97e49a275b
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-07-21 02:16:54 -0700

    Added defaultStrategy property

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { isEmpty, isString, isObject, isArray, trim } from 'lodash';
 export class Accounts {
   constructor() {
     this.strategies = {};
+    this.defaultStrategy = {};
   }
   addStrategy(name, args) {
     this.strategies[name] = args;
@@ -13,12 +14,6 @@ export class Accounts {
   deserializeUser(object, callback) {
     return callback(null, object);
   }
-  /**
-   * Returns a promise that calls Passport's verify callback with the results this.authenticate
-   * @param  {...object} ...args An array of objects, the last of which must be Passport's
-   * verify callback.
-   * @return Promise
-   */
   verify(done, name, args) {
   }
 }
